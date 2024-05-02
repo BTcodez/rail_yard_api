de27d1227294c133558aef71e27254aa452d276a
sdlvlamp3 <sdlviola@gmail.com>
2024-05-02 17:38:34 -0500

    Validations Completed

--- app/models/raw_material.rb
+++ app/models/raw_material.rb
@@ -1,3 +1,13 @@
 class RawMaterial < ApplicationRecord
+#Validations:
+    validates :material_name, 
+        presence: true, 
+        length: { minimum: 1, maximum: 255 }
+  
+    validates :weight, 
+        presence: true, 
+        numericality: { greater_than_or_equal_to: 0 }
+ 
+#Associations:
     has_many :orders
 end
