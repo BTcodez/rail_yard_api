275a0953295e4976ab49e498dea7ddcf71c93bc1
sdlvlamp3 <sdlviola@gmail.com>
2024-05-06 19:35:30 -0500

    user model test completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,51 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     let (:user) { build(:user) }
 
-    context 'validation tests' do
+    context 'presence validation tests' do
         it 'is valid' do
             expect(user).to be_valid
         end
 
         it 'is not valid without a name' do
-            build(:user, name: nil)
+            user = build(:user, name: nil)
             expect(user).to_not be_valid
         end
+
+        it 'is not valid without an email' do
+            user = build(:user, email: nil)
+            expect(user).to_not be_valid
+        end
+
+        it 'is not valid without an password_digest' do
+            user = build(:user, password_digest: nil)
+            expect(user).to_not be_valid
+        end
+    end
+
+    context 'with valid email attributes' do
+        it 'is invalid with a duplicate email' do
+          create(:user, email: "john@example.com")
+          user = build(:user, email: "john@example.com")
+          expect(user).to_not be_valid
+          expect(user.errors[:email]).to include("has already been taken")
+        end
+
+        it 'is invalid with an invalid email' do
+            user = build(:user, email: :"testemail.com")
+            expect(user).to_not be_valid
+        end
+    end
+
+    context 'with valid name attributes' do
+        it 'is invalid due to name formatting' do
+            user = build(:user, name: "Rev. John Murdock")
+            expect(user).to_not be_valid
+        end
+    end
+
+    context 'with valid password attributes' do
+        it 'is invalid when the password is below 10 characters.'
+        user = build(:user, password_digest: '1j5hv73')
+        expect(user).to_not be_valid
     end
 end
