aaaf31edce650b9d1546630cccf04a7d488ce427
Brian Tuohy <BTcodez22@gmail.com>
2024-05-06 20:52:16 -0500

    updated user model/controller for create method. added credentials to gitignore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.new(user_params)
+    user = User.create(user_params)
     if user.save
-      render json: UserBlueprint.render(user, view: :extended), status: :created
+      render json: user, status: :created
     else
       render json: user.errors, status: :unprocessable_entity
     end
@@ -44,5 +44,5 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:name, :email, :password_digest, :user_type)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :user_type)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
 #Validations:
     validates :name, 
         presence: true, 
@@ -10,9 +11,9 @@ class User < ApplicationRecord
         uniqueness: true, 
         format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }    
 
-    validates :password_digest, # CHANGE password_digest
-        presence: true, 
-        length: { minimum: 10 } #Adjust length as needed.
+    # validates :password_digest, # CHANGE password_digest
+    #     presence: true, 
+    #     length: { minimum: 10 } #Adjust length as needed.
 
         validates :user_type, 
             presence: true 
