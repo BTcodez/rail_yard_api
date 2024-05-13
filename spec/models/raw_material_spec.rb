c0ce62d6c1fd15818b96a1fe39f60b491652402a
sdlvlamp3 <sdlviola@gmail.com>
2024-05-13 18:37:52 -0500

    rspec models fixed

--- spec/models/raw_material_spec.rb
+++ spec/models/raw_material_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-Rspec.describe RawMaterial, type: :model do
+RSpec.describe RawMaterial, type: :model do
     let (:raw_material) { build(:raw_material) }
 
     context 'with valid presence attributes' do
         it 'is valid' do
-            expect(:raw_material).to be_valid
+            expect(raw_material).to be_valid
         end
 
         it 'is not valid without a material_name' do
