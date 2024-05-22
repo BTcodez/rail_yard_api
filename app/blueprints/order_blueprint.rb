8892b45843fefc0004aebde332bff9091f6b8bc7
Johnny M <j2mell@yahoo.com>
2024-05-21 19:08:28 -0500

    feat: Add raw_material_id field to OrderBlueprint

--- app/blueprints/order_blueprint.rb
+++ app/blueprints/order_blueprint.rb
@@ -2,7 +2,7 @@
 
 class OrderBlueprint < Blueprinter::Base
     identifier :id
-        fields :car_id, :requested_date, :received_date, :extraction_start, :extraction_end, :release_date, :weight
+        fields :car_id, :requested_date, :received_date, :extraction_start, :extraction_end, :release_date, :weight, :raw_material_id
     view :normal do
         association :user, blueprint: UserBlueprint, view: :normal
         association :raw_material, blueprint: RawMaterialBlueprint, view: :normal
